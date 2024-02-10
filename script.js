e5015ee40870fbecc46f870647e3912d73ab9767
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-02-10 11:48:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+	name: "john"
+};
+
+//object prototpye method add
+Object.prototype.getKeys() = function(){
+	return object.keys(this);
+};
+
+console.log(student.getKeys());
